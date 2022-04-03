dc8dae0616eee8194a351f10fedbb9d11108ec4a
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-04-02 17:08:24 -0700

    Workbook 2.5 solved.

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -9,20 +9,42 @@ public class SignUp {
         
 
         //Task 1 - Using Scanner, ask the user questions
-
         //Ask for their first name.
+        System.out.println("What is your name?");
+        String name =  scan.nextLine();
+        
         //Ask for their last name.
+        System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
+        
         //Ask: how old are you?
+        System.out.println("How old are you?");
+        String age = scan.nextLine();
+        
         //Ask them to make a username.
+        System.out.println("What should be your username?");
+        String username = scan.nextLine();
+        
         //Ask what city they live in.
+        System.out.println("What city are you live in?");
+        String city = scan.nextLine();
+        
         //Ask what country that's from.
-
-
+        System.out.println("Which country is your city from?");
+        String country = scan.nextLine();
+        
         //Task 2 - Print their information. 
 
         System.out.println("Thank you for joining JavaGram!");
 
         //Print their information like so:
+        System.out.println("Your information: ");
+        System.out.println("\t First Name: " + name);
+        System.out.println("\t Last Name: " + lastName);
+        System.out.println("\t Age: " + age);
+        System.out.println("\t Username: " + username);
+        System.out.println("\t City: " + city);
+        System.out.println("\t Country: " + country);
 
         // Your information:
         //        First Name: Rayan
@@ -34,6 +56,7 @@ public class SignUp {
         //
 
 
-        //close scanner. It's good practice :D ! 
+        //close scanner. It's good practice :D !
+        scan.close(); 
     }
 }
