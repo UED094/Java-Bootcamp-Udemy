70dab9d91c90b384e2b430f7d3efdc87e5e8e1a9
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-04-07 00:44:21 -0700

    Workbook 6.4 solved.

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.4/Grocer.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.4/Grocer.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Grocer {
     public static void main(String[] args) {
 
@@ -5,7 +7,8 @@ public class Grocer {
         System.out.println("\nWelcome to Java Grocers. ");
         System.out.println("What can I help you find?\n");
         //  Task 1: Set up Scanner, and pick up the user's response.  
-
+        Scanner scan = new Scanner(System.in);
+        String response = scan.nextLine();
         
         for (int i = 0; i < store.length; i++) {
           /* Task 2  
@@ -15,6 +18,13 @@ public class Grocer {
                 }
           */
         }
+        for (int i = 0; i < store.length; i++) {
+            if (store[i].equals(response)) {
+                System.out.println("\nWe have that in aisle: " + i);
+                break;
+            }
+        }
+        scan.close();
 
     }
 }
