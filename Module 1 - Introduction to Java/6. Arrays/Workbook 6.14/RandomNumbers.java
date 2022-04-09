905c0757035acd23d234ef8ea90c65ce786efda6
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-04-08 23:20:49 -0700

    Workbook 6.14 solved.

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
@@ -1,7 +1,9 @@
+import java.util.Random;
+
 public class RandomNumbers {
     public static void main(String[] args) {
         // Task 3 – Create a 2D array with 100 rows and 10 columns.
-
+        int[][] array = new int[100][10];
 
         /** Task 4
          * 
@@ -9,8 +11,12 @@ public class RandomNumbers {
          * 2. Then, pass the updated array into print2DArray.  
          * 
          */
-
-
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = randomNumber();
+            }
+        }
+        print2DArray(array);
         
     }
 
@@ -22,7 +28,10 @@ public class RandomNumbers {
      * Inside the function:
      *    1. return a random number between 1 and 100. 
      */
-
+    public static int randomNumber() {
+        int randomNumber = (int)(Math.random() * 100);
+        return randomNumber;
+    }
 
     /** Task 2
      * Function name: print2DArray
@@ -34,4 +43,12 @@ public class RandomNumbers {
      *        • Each row takes up one line.
      *        • There's a space between each value.
      */
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(String.format("%3d", array[i][j]));
+            }
+            System.out.println();
+        }
+    }
 }
