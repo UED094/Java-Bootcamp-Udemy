b1f23b23e4c6b85dfb942032eecbd7539c44fd0d
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-04-10 22:49:43 -0700

    Airline-reservation-system: Solved part 9.

--- Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Airline.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Airline.java
@@ -1,7 +1,38 @@
-public class Airline {
-    Person[] seats;
+import java.net.SocketTimeoutException;
 
-    // public static void createReservation() {
+public class Airline {
+    private Person[] seats;
+
+    public Airline() {
+        this.seats = new Person[11];
+    }
+
+    public Person getPerson(int index) {
+        Person person = new Person(this.seats[index]);
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.seats[person.getSeatNumber()-1] = new Person(person);
+    }
+
+    public void createReservation(Person person) {
+        while (this.seats[person.getSeatNumber()-1] != null) {
+            
+            System.out.println("\n" + person.getName() + ", seat: " +person.getSeatNumber()+ " is already taken. Please choose another one.\n");
+            person.chooseSeat();
+        }
+        this.seats[person.getSeatNumber()-1] = new Person(person);
+        System.out.println("Thank you " + person.getName() + " for flying with Java airlines. Your seat number is " + person.getSeatNumber() + ".\n");
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < seats.length; i++) {
+            temp += seats[i].toString();
+            temp += "\n\n";
+        }
+        return temp;
         
-    // }
+    }
 }
--- Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -1,13 +1,26 @@
 import java.util.Arrays;
 public class Main {
+    public static void main(String[] args) {                                  
 
-    public static void main(String[] args) {
-
-        Person person = new Person("Ugur Emre Dogan", "Turkish", "05/11/1994", 6); 
-        if (person.applyPassport() == true) {
-            person.setPassport();
+        Person[] people = new Person[] {                                       
+            new Person("Cleopatra", "Egypt", "69 BC", 1),                      
+            new Person("Alexander the Great", "Macedon", "356 BC", 2),
+            new Person("Julius Caesar", "Rome", "100 BC", 3),
+            new Person("Hannibal", "Carthage", "247 BC", 4),                   
+            new Person("Confucius", "China", "551 BC", 5),                     
+            new Person("Pericles", "Greece", "429 BC", 6),                     
+            new Person("Spartacus", "Thrace", "111 BC", 7),                    
+            new Person("Marcus Aurelius", "Rome", "121 AD", 8),
+            new Person("Leonidas", "Greece", "540 BC", 9),                     
+            new Person("Sun Tzu", "China", "544 BC", 10),                      
+            new Person("Hammurabi", "Babylon", "1750 BC", 10),
+        };
+        Airline airline = new Airline();                                      
+        for (int i = 0; i < people.length; i++) {
+            airline.createReservation(people[i]);
         }
-
-        System.out.println(person);
-    }
+        
+        System.out.println("****************** RESERVATIONS COMPLETE! ******************\n");
+        System.out.println(airline);
+    }      
 }
