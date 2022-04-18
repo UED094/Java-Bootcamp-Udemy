85555b57fba7cea10983fc74ec406e7646239338
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-04-17 20:15:07 -0700

    Vending Machine: Task 2 is finished.

--- Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class Machine {
+    Item items[][] = new Item[3][3];
+
+    public Machine (Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
     
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
     
     
     /**
@@ -13,4 +33,13 @@ public class Machine {
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+    public boolean dispense(int row, int spot) {
+        if (this.items[row][spot].getQuantity() > 0 ) {
+            this.items[row][spot].setQuantity(this.items[row][spot].getQuantity() - 1);
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
 }
--- Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Main.java
@@ -5,13 +5,17 @@ public class Main {
         System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
         System.out.println("\t************************************************");
         
-        /* Item[][] items = new Item[][] {
-        /     { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
-        /     { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
-        /     { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
-        / };
-        */
-        Item test = new Item("Pepsi", 1.99, 3);
-        System.out.println(test);
+        Item[][] items = new Item[][] {
+            { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
+            { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
+            { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
+        };
+        
+        Machine machine = new Machine(items);
+        Item testItem = machine.getItem(2,1);
+        testItem.setPrice(2.99);
+        System.out.println(machine.getItem(2,1));
+        machine.dispense(2,1);
+        System.out.println(machine.getItem(2,1));
     }
 }
