68ceb8b24ff4eb48877497ac1ff02900eb2ed3a6
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-19 16:56:54 -0700

    Exception Handling: Contacts part 2 solved.

--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
@@ -1,33 +1,47 @@
+import java.text.ParseException;
+
+import org.w3c.dom.css.CSS2Properties;
+
+import models.Contact;
+
 public class Main {
     public static void main(String[] args) {
-        
-      
-        
+        try {
+            Contact contact = new Contact("Ugur", "5325555555", "05/11/1994");
+            System.out.println(contact);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Process Complete.");
+        }
+
     }
 
     /**
      * Name: manageContacts
      *
      * Inside the function:
-     *   • 1. Starts a new instance of Scanner;
-     *   • 2. In an infinite loop, the user can choose to a) add b) remove a contact c) exit.
-     *   •        case a: ask for the name, phone number and birthDate.
-     *   •        case b: ask who they'd like to remove.
-     *   •        case c: break the loop.
-     *   • 3. close Scanner.
+     * • 1. Starts a new instance of Scanner;
+     * • 2. In an infinite loop, the user can choose to a) add b) remove a contact
+     * c) exit.
+     * • case a: ask for the name, phone number and birthDate.
+     * • case b: ask who they'd like to remove.
+     * • case c: break the loop.
+     * • 3. close Scanner.
      */
-
-
 
     /**
      * Name: loadContacts
+     * 
      * @param fileName (String)
      * @throws FileNotFoundException
      *
-     * Inside the function:
-     *   • 1. loads contacts from <fileName>;
-     *   • 2. From the manager object, it adds all contacts to the contacts list.
-     *        Hint: use scan.next to grab the next String separated by white space.
+     *                               Inside the function:
+     *                               • 1. loads contacts from <fileName>;
+     *                               • 2. From the manager object, it adds all
+     *                               contacts to the contacts list.
+     *                               Hint: use scan.next to grab the next String
+     *                               separated by white space.
      */
 
 }
--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/models/Contact.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/models/Contact.java
@@ -1,8 +1,93 @@
 package models;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.chrono.ThaiBuddhistChronology;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Contact {
-  
-  
-  
-  
+    private String name;
+    private int age;
+    private String birthDate;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber, String birthDate) throws ParseException {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.age = toAge(birthDate);
+    }
+
+    public Contact(Contact source) {
+        this.name = source.name;
+        this.birthDate = source.birthDate;
+        this.phoneNumber = source.phoneNumber;
+        this.age = source.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBirthDate(String birthDate) throws ParseException {
+        this.birthDate = birthDate;
+        setAge(toAge(birthDate));
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * Name: toAge
+     * 
+     * @param birthDate
+     * @return age (int)
+     * @throws ParseException
+     *
+     *                        Inside the function:
+     *                        1. Parses a date from the birthDate String
+     *                        2. Gets the current date
+     *                        3. Subtracts the difference and returns the age.
+     *
+     */
+    private int toAge(String birthDate) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(birthDate);
+        formatter.setLenient(false);
+        long diff = new Date().getTime() - formatter.parse(birthDate).getTime();
+        int age = (int) (TimeUnit.MILLISECONDS.toDays(diff) / 365);
+        return age;
+    }
+
+    public String toString() {
+        return "Name: " + this.getName() + "\n" +
+
+                "Phone number: " + this.getPhoneNumber() + "\n" +
+
+                "Birth Date: " + this.getBirthDate() + "\n" +
+
+                "Age: " + this.getAge() + " year old\n";
+    }
 
 }
