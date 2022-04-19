d5a3f9313081a00772ef71ffbbdb11eed33f1c37
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-18 19:03:26 -0700

    Debugging Exercise 10 solved.

--- Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-ten/Transpose.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-ten/Transpose.java
@@ -3,22 +3,25 @@ import java.util.Arrays;
 public class Transpose {
     public static void main(String[] args) {
         int[][] matrix = new int[][] {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
+
+        int[][] transposedMatrix = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = matrix[j][i];
+                transposedMatrix[j][i] = matrix[i][j];
             }
         }
 
         System.out.println();
-        printMatrix(matrix);
+        printMatrix(transposedMatrix);
     }
+
     public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-                System.out.println(Arrays.toString(matrix[i]));
+            System.out.println(Arrays.toString(matrix[i]));
         }
     }
 }
