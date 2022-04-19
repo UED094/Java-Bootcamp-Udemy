f0cd8ff4b59d7b4fd1a9d14d0aaeb5bc1e41d42e
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-18 18:17:10 -0700

    Debugging Exercise 5 solved.

--- Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-five/Pyramid.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-five/Pyramid.java
@@ -1,18 +1,18 @@
 public class Pyramid {
     public static void main(String[] args) {
         char letter = 'a';
-            
+
         for (int i = 1; i < 10; i++) {
             if (i % 2 == 0) {
                 System.out.print("\n");
                 continue;
             }
-            for (int j = 1; j <= 10; j++) {
-                letter++;
-                if (j < i) {
-                    System.out.print("   ");
-                } else {
+            for (int j = 1; j <= i; j++) {
+                if (j == i) {
                     System.out.print(" " + letter + " ");
+                    letter++;
+                } else {
+                    System.out.print("   ");
                 }
             }
             System.out.print("\n");
