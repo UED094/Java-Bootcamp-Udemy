2415414cf3d7a0ad9aba8235f62a7ce8d7afcc15
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-20 17:14:48 -0700

    Exception Handling: Contacts part 7 solved.

--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
@@ -1,32 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.text.ParseException;
-
-import org.w3c.dom.css.CSS2Properties;
+import java.util.Scanner;
 
 import models.Contact;
 import models.ContactManager;
 
 public class Main {
-    public static void main(String[] args) {
-        ContactManager contactManager = new ContactManager();
+    static ContactManager contactManager = new ContactManager();
 
+    public static void main(String[] args) {
         try {
-            Contact ryan = new Contact("Ryan", "6135012424", "11/11/1992");
-            Contact gio = new Contact("Gio", "6477092344", "11/11/1993");
-            Contact thomas = new Contact("Thomas", "8192256979", "11/11/1994");
-            contactManager.addContact(ryan);
-            contactManager.addContact(gio);
-            contactManager.addContact(thomas);
-        } catch (ParseException e) {
+            loadContacts("contacts.txt");
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println("Process Complete.");
+            System.out.println("CONTACTS LOADED\n\n");
+            System.out.println(contactManager);
+            manageContacts();
         }
-
-        System.out.println(contactManager);
-        contactManager.removeContact("Gio");
-        System.out.println("************ NEW LIST **********");
-        System.out.println(contactManager);
-
     }
 
     /**
@@ -42,6 +34,48 @@ public class Main {
      * • 3. close Scanner.
      */
 
+    public static void manageContacts() {
+        Scanner scan = new Scanner(System.in);
+        loopManageContacts: while (true) {
+            System.out.println("Would you like to \n\ta) add another contact\n\tb) remove a contact \n\tc) exit");
+            switch (scan.nextLine()) {
+                case "a":
+                    System.out.print("\tName: ");
+                    String name = scan.nextLine();
+                    System.out.print("\tPhone Number: ");
+                    String phoneNumber = scan.nextLine();
+                    System.out.print("\tBirth Date: ");
+                    String birthDate = scan.nextLine();
+                    if (name == null || name.isBlank() || phoneNumber == null || phoneNumber.isBlank()
+                            || phoneNumber.length() < 5) {
+                        System.out.println("\nThe input you provided is not valid. Registration failed.");
+                    }
+
+                    try {
+                        contactManager.addContact(new Contact(name, phoneNumber, birthDate));
+                    } catch (ParseException e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+                        System.out.println("\n\nUPDATED CONTACTS\n\n");
+                        System.out.println(contactManager);
+                    }
+                    break;
+                case "b":
+                    System.out.println("\nWho would you like to remove?");
+                    contactManager.removeContact(scan.nextLine());
+                    System.out.println("\n\nUPDATED CONTACTS\n\n");
+                    System.out.println(contactManager);
+                    break;
+                case "c":
+                    break loopManageContacts;
+                default:
+                    System.out.println("Invalid Choice please choose a valid option.");
+                    break;
+            }
+
+        }
+    }
+
     /**
      * Name: loadContacts
      * 
@@ -55,5 +89,18 @@ public class Main {
      *                               Hint: use scan.next to grab the next String
      *                               separated by white space.
      */
+    public static void loadContacts(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanFile = new Scanner(fis);
+        while (scanFile.hasNextLine()) {
+            try {
+                Contact contact = new Contact(scanFile.next(), scanFile.next(), scanFile.next());
+                contactManager.addContact(contact);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        scanFile.close();
+    }
 
 }
