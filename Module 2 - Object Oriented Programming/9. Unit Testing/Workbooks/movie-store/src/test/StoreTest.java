517806b4616baf2863bc450265f6fda2b7b16146
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-22 12:13:05 -0700

    Unit Testing: Movie store: all parts are solved.

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/test/StoreTest.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/test/StoreTest.java
@@ -2,8 +2,48 @@ package src.test;
 
 import src.main.models.Store;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import src.main.models.Movie;
+
 public class StoreTest {
 
     Store store;
+
+    @Before
+    public void setup() {
+        store = new Store();
+        store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+        store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+    }
+
+    @Test
+    public void movieAdded() {
+        assertTrue(store.contains(new Movie("The Shawshank Redemption", "Blue-Ray",
+                9.2)));
+    }
+
+    @Test
+    public void sellMovieTest() {
+        store.sellMovie("The Godfather");
+        assertFalse(store.contains(new Movie("The Godfather", "Blue-Ray", 9.1)));
+    }
+
+    @Test
+    public void rentMovieTest() {
+        store.rentMovie("The Godfather");
+        assertFalse(store.getMovie(1).isAvailable());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void movieNotInStock() {
+        store.rentMovie("The Godfather");
+        store.rentMovie("The Godfather");
+
+    }
 
 }
