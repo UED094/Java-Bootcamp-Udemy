ab075db8ba93502ca43f3d902eb758ddb19ef1eb
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-23 00:37:41 -0700

    Unit Testing: Quidditch Game Challenge - Part 1 finished.

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
@@ -1,29 +1,82 @@
 package src.main.models;
 
+import java.util.Arrays;
+
 public class Team {
+
+    private String house;
+    private String keeper;
+    private String seeker;
+    private String[] chasers;
 
     private static final String POSITION_CHASER = "chaser";
     private static final String POSITION_SEEKER = "seeker";
     private static final String POSITION_KEEPER = "keeper";
 
-/* FREQUENTLY ASKED QUESTIONS:
-    
-Question: the constants are final, so why can't we make them public? It's not possible for the caller to update them.
-  Answer: Even if the constant is final, I prefer to expose a method instead of the variable. But if you want to expose the variable, that's also correct.
+    public Team(String house, String keeper, String seeker, String[] chasers) {
+        this.house = house;
+        this.keeper = keeper;
+        this.seeker = seeker;
+        this.chasers = chasers.clone();
+    }
 
-*/
+    public Team(Team source) {
+        this.house = source.house;
+        this.keeper = source.keeper;
+        this.seeker = source.seeker;
+        this.chasers = source.chasers.clone();
+    }
 
-    
-     public static String getPositionChaser() {
-         return POSITION_CHASER;
-     }
+    public String getHouse() {
+        return this.house;
+    }
 
-     public static String getPositionSeeker() {
-         return POSITION_SEEKER;
-     }
+    public void setHouse(String house) {
+        this.house = house;
+    }
 
-     public static String getPositionKeeper() {
-         return POSITION_KEEPER;
-     }
+    public String getKeeper() {
+        return this.keeper;
+    }
+
+    public void setKeeper(String keeper) {
+        this.keeper = keeper;
+    }
+
+    public String getSeeker() {
+        return this.seeker;
+    }
+
+    public void setSeeker(String seeker) {
+        this.seeker = seeker;
+    }
+
+    public String[] getChasers() {
+        return this.chasers.clone();
+    }
+
+    public void setChasers(String[] chasers) {
+        this.chasers = chasers.clone();
+    }
+
+    public static String getPositionChaser() {
+        return POSITION_CHASER;
+    }
+
+    public static String getPositionSeeker() {
+        return POSITION_SEEKER;
+    }
+
+    public static String getPositionKeeper() {
+        return POSITION_KEEPER;
+    }
+
+    @Override
+    public String toString() {
+        return "House: " + this.house + "\n" +
+                "Keeper: " + this.keeper + "\n" +
+                "Seeker: " + this.seeker + "\n" +
+                "Chasers: " + Arrays.toString(this.chasers) + "\n";
+    }
 
 }
