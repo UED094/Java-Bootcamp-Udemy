ae41013355dabdd7935a60bfd6080d73d0d9f4e3
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-23 23:22:43 -0700

    Unit Testing: Quidditch Game Challenge - Part 7 finished.

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/Main.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/Main.java
@@ -1,20 +1,38 @@
 package src.main;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Time;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import javax.xml.validation.Schema;
+
 import src.main.models.Game;
 import src.main.models.Team;
 
 public class Main {
 
         static Game game;
+        static final String TEAMS_FILE = "src/main/teams.txt";
+        static final String PLAYS_FILE = "src/main/plays.txt";
 
         public static void main(String[] args) {
 
-                game = new Game(new Team("GRYFFINDOR", "Oliver", "Harry",
-                                new String[] { "Angelina", "Ginny", "Katie" }),
-                                new Team("SLYTHERIN", "Vincent", "Draco",
-                                                new String[] { "Bridget", "Harper", "Malcolm" }));
-                System.out.println(game.simulate(
-                                "<chaser> starts with the quaffle, speeds off towards the goal posts, and scores! 10 points!"));
+                try {
+                        String[][] data = getData();
+                        game = new Game(
+                                        new Team(data[0][0], data[0][1], data[0][2],
+                                                        new String[] { data[0][3], data[0][4], data[0][5] }),
+                                        new Team(data[1][0], data[1][1], data[1][2],
+                                                        new String[] { data[1][3], data[1][4], data[1][5] }));
+                        startGame();
+                        printResult();
+                } catch (FileNotFoundException e) {
+                        System.out.println(e.getMessage());
+                }
+
         }
 
         /**
@@ -27,6 +45,13 @@ public class Main {
          *                               1. Returns data from TEAMS_FILE as a String[][]
          *                               array
          */
+        public static String[][] getData() throws FileNotFoundException {
+                FileInputStream fis = new FileInputStream(TEAMS_FILE);
+                Scanner scanFile = new Scanner(fis);
+                String[] lines = new String[] { scanFile.nextLine(), scanFile.nextLine() };
+                scanFile.close();
+                return new String[][] { lines[0].split(","), lines[1].split(",") };
+        }
 
         /**
          * Function name: startGame
@@ -36,6 +61,15 @@ public class Main {
          * 2. Prints the return from game.simulate(play)
          * - println("\n" + <return> + "\n");
          */
+        public static void startGame() throws FileNotFoundException {
+                FileInputStream fis = new FileInputStream(PLAYS_FILE);
+                Scanner scanFile = new Scanner(fis);
+                while (scanFile.hasNextLine()) {
+                        System.out.println("\n" + game.simulate(scanFile.nextLine()) + "\n");
+                        wait(3);
+                }
+
+        }
 
         /**
          * Function name: printResult()
@@ -46,6 +80,15 @@ public class Main {
          * 2. Prints the winner: println("\n" + <winner team name> + " WINS!");
          * 
          */
+        public static void printResult() {
+                Team gryffindor = game.getTeam("GRYFFINDOR");
+                Team slytherin = game.getTeam("SLYTHERIN");
+                Team winner = game.getScore(gryffindor) > game.getScore(slytherin) ? gryffindor : slytherin;
+                System.out.println("\nGRYFFINDOR: " + game.getScore(gryffindor) + " SLYTHERIN: "
+                                + game.getScore(slytherin));
+                System.out.println("\n" + winner.getHouse() + "WINS!!!");
+
+        }
 
         /**
          * Function name: wait
@@ -55,5 +98,13 @@ public class Main {
          *            Inside the function:
          *            1. Make the code sleep for X seconds.
          */
+        public static void wait(int sec) {
+                try {
+                        TimeUnit.SECONDS.sleep(sec);
+                } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                }
+
+        }
 
 }
