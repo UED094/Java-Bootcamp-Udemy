239a31b72077af7b9b5ea349996dea6ca7e7b0a2
Ugur Dogan <ugur.dogan@resmed.com>
2022-04-25 10:11:52 -0700

    Concurrency: Workbook 1.1 part 1 done.

--- Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.1/Main.java
+++ Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.1/Main.java
@@ -1,20 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.Scanner;
 
 public class Main {
 
-    static final String SALES = "data/sales.csv"; //Use backslash Windows users
+    static final String SALES = "data/sales.csv"; // Use backslash Windows users
 
     public static void main(String[] args) {
-        
+
         try {
             Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(SALES).toURI());
-            //calculate average sales of "Furniture" here
-            //calculate average sales of "Technology" here
-            //calculate average sales of "Office Supplies" here
-            //calculate total average of sales here
+            // calculate average sales of "Furniture" here
+            // calculate average sales of "Technology" here
+            // calculate average sales of "Office Supplies" here
+            // calculate total average of sales here
 
         } catch (URISyntaxException e) {
             System.out.println(e.getMessage());
@@ -23,34 +26,50 @@ public class Main {
 
     /**
      * Function name: average
-     * @param path (Path)
+     * 
+     * @param path     (Path)
      * @param category (String)
      * @return Double
      * 
-     * Inside the function:
-     *   1. Runs through every line from the CSV file as a stream.
-     *   2. Maps every element in the stream to an array of three String values.
-     *   3. Filters every value by the @param category
-     *   4. Maps every element in the stream to a double (price * quantity).
-     *   5. Applies the terminal operation average.
-     *   6. Returns the average as double.
+     *         Inside the function:
+     *         1. Runs through every line from the CSV file as a stream.
+     *         2. Maps every element in the stream to an array of three String
+     *         values.
+     *         3. Filters every value by the @param category
+     *         4. Maps every element in the stream to a double (price * quantity).
+     *         5. Applies the terminal operation average.
+     *         6. Returns the average as double.
      * 
      */
+    public static double average(Path path, String category) {
 
+        try {
+            return Files.lines(path).skip(1)
+                    .map((line) -> line.split(","))
+                    .filter((line) -> line[0].equals(category))
+                    .mapToDouble((values) -> Double.parseDouble(values[1]) * Double.parseDouble(
+                            values[2]))
+                    .average()
+                    .getAsDouble();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0.0;
+        }
 
+    }
 
     /**
      * Function name: totalAverage
+     * 
      * @param path (Path)
      * @return Double
      * 
-     * Inside the function:
-     *   1. Runs through every line from the CSV file as a stream.
-     *   2. Maps every element in the stream to an array of three values.
-     *   3. Maps every element in the stream to a double: (price * quantity).
-     *   4. Applies the terminal operation average.
-     *   5. Returns the average as double.
+     *         Inside the function:
+     *         1. Runs through every line from the CSV file as a stream.
+     *         2. Maps every element in the stream to an array of three values.
+     *         3. Maps every element in the stream to a double: (price * quantity).
+     *         4. Applies the terminal operation average.
+     *         5. Returns the average as double.
      */
-  
 
 }
